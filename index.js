e728474d6ebb7afd0a1c03c6f2b954cf7833f88e
AbbasLewa <abbasomerhassn@gmail.com>
2025-12-29 11:30:51 +0300

    add

--- index.js
+++ index.js
@@ -3,46 +3,26 @@ import moment from "moment";
 import jsonfile from "jsonfile";
 import simpleGit from "simple-git";
 
-const git = simpleGit();
-const FILE_PATH = "./data.json";
+const path = "./data.json";
 
-// CONFIG
-const TOTAL_COMMITS = 300;
-const MAX_COMMITS_PER_DAY = 3;
+const makeCommits = async (n) => {
+    if (n === 0) return simpleGit().push();
 
-const usedDates = {};
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
+    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
 
-const randomDateLastYear = () => {
-  const weeks = random.int(0, 52);
-  const days = random.int(0, 6);
+    const data = { date };
 
-  return moment()
-    .subtract(1, "year")
-    .add(weeks, "weeks")
-    .add(days, "days")
-    .startOf("day")
-    .format();
+    console.log(date);
+
+    jsonfile.writeFileSync(path, data);
+
+    await simpleGit()
+        .add([path])
+        .commit("date", { "--date": date });
+
+    await makeCommits(--n);
 };
 
-const makeCommits = async (count) => {
-  for (let i = 0; i < count; i++) {
-    let date;
-
-    // Ensure realistic daily limits
-    do {
-      date = randomDateLastYear();
-      usedDates[date] = (usedDates[date] || 0) + 1;
-    } while (usedDates[date] > MAX_COMMITS_PER_DAY);
-
-    jsonfile.writeFileSync(FILE_PATH, { date });
-
-    await git.add(FILE_PATH);
-    await git.commit("chore: update data", { "--date": date });
-
-    console.log(`✔ Commit ${i + 1}/${count} → ${date}`);
-  }
-
-  await git.push();
-};
-
-makeCommits(TOTAL_COMMITS).catch(console.error);
+makeCommits(500);
