a672f95a020ed8c875f07e6882125279cbf79c9d
AbbasLewa <abbasomerhassn@gmail.com>
2026-04-20 13:14:06 +0300

    add pic

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-import random from "random";
-import moment from "moment";
 import jsonfile from "jsonfile";
+import moment from "moment";
 import simpleGit from "simple-git";
+import random from "random";
 
 const path = "./data.json";
 
-const makeCommits = async (n) => {
-    if (n === 0) return simpleGit().push();
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
 
-    const x = random.int(0, 54);
-    const y = random.int(0, 6);
-    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  const data = {
+    date: date,
+  };
 
-    const data = { date };
-
-    console.log(date);
-
-    jsonfile.writeFileSync(path, data);
-
-    await simpleGit()
-        .add([path])
-        .commit("date", { "--date": date });
-
-    await makeCommits(--n);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
 };
 
-makeCommits(1000);
+const makeCommits = (n) => {
+  if(n===0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+  const data = {
+    date: date,
+  };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+  });
+};
+
+makeCommits(100);
